f19304451dc8bce5e150b291c352a395a0eea942
Josh Bivins <bivinsj553@gmail.com>
2026-07-23 09:33:37 -0400

    v1.8.1.2 (Beta) Windows Expansion

--- config.h
+++ config.h
@@ -1,10 +1,12 @@
 // ============Commands=============
 // Define your command headers here
+#ifndef _WIN32
 #include "jim_commands/jim_shell.h"
 #include "jim_commands/jim_cat.h"
 #include "jim_commands/jim_open.h"
 #include "jim_commands/jim_viewsyn.h"
 #include "jim_commands/jim_select.h"
+#endif
 // =================================
 
 typedef int (*CommandFunc)(const int argc, const char* args[]); //By default, 0 will be interpretted as success and any negative values are errors which will be printed
